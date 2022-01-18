7f1294bf1c898fba835c99bf7add9bd0b21896f0
shvmsh20 <itsshivamsharma21@gmail.com>
2022-01-18 22:46:37 +0530

    Day1: Q2

---
+++ Day-1/Pascal's_Triangle.cpp
@@ -0,0 +1,12 @@
+vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v(numRows);
+        for(int i=0; i<numRows; i++){
+            v[i].resize(i+1);
+            //first and last element of each row should be 1
+            v[i][0] = v[i][i] = 1;
+            for(int j=1; j<i; j++){
+                v[i][j] = v[i-1][j-1]+v[i-1][j];
+            }
+        }
+        return v;
+    }
