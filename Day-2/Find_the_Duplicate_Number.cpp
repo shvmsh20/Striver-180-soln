9cce744d4704019efeb19937f679ce9a4ec82d11
shvmsh20 <itsshivamsharma21@gmail.com>
2022-01-21 11:34:25 +0530

    Day2: Q4

---
+++ Day-2/Find_the_Duplicate_Number.cpp
@@ -0,0 +1,16 @@
+int findDuplicate(vector<int>& nums) {
+        int fast = nums[0], slow = nums[0];
+        //find collision point
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+
+        //find loop starting point or duplicate number
+        slow = nums[0];
+        while(fast!=slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
