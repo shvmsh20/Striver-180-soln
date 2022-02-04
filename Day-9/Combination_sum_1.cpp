8dc4a8867cba60e323e190d7c0fcabe434f9f585
shvmsh20 <itsshivamsharma21@gmail.com>
2022-02-05 00:43:17 +0530

    update:Day9-Q3

--- Day-9/Combination_sum_1.cpp
+++ Day-9/Combination_sum_1.cpp
@@ -1,3 +1,28 @@
+//T/C: O(pow(2,t)*k)
+void util(vector<int>& candidates, int target, int index, vector<int> &v,
+          vector<vector<int>> &res){
+        if(index==candidates.size()){
+            if(target==0){
+                res.push_back(v);
+            }
+            return;
+        }
+        if(target>=candidates[index]){
+            v.push_back(candidates[index]);
+            util(candidates, target-candidates[index], index, v, res);
+            v.pop_back();
+        }
+        util(candidates, target, index+1, v, res);
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> v;
+        vector<vector<int>> res;
+        util(candidates, target, 0, v, res);
+        return res;
+    }
+
+//OR
 void util(vector<int>& candidates, int target, int index, vector<int> &v,
               vector<vector<int>> &res, int sum){
         if(sum==target){
