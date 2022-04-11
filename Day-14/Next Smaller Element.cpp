b918b92b85c18778f54cf6cf0def50c043e94a49
shvmsh20 <itsshivamsharma21@gmail.com>
2022-04-11 18:24:59 +0530

    update Day14: Q1

--- Day-14/Next Smaller Element.cpp
+++ Day-14/Next Smaller Element.cpp
@@ -1,3 +1,4 @@
+//Next Greater 1
 vector<int> Solution::prevSmaller(vector<int> &A) {
     stack<int> st;
     int n = A.size();
@@ -22,3 +23,25 @@ vector<int> Solution::prevSmaller(vector<int> &A) {
     return v;
 
 }
+
+//Next Greater Element II
+vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n = nums.size();
+        vector<int> res(n);
+        for(int i=n-1; i>=0; i--){
+            st.push(nums[i]);
+        }
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && st.top()<=nums[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                res[i] = -1;
+            }else{
+                res[i] = st.top();
+            }
+            st.push(nums[i]);
+        }
+        return res;
+    }
