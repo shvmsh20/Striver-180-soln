986fb7c7ea7477d5dafa419e9d31e9abb1954d68
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-03 22:39:29 +0530

    update: Day 19: Q7

--- Day-19/Check for Children Sum Property.cpp
+++ Day-19/Check for Children Sum Property.cpp
@@ -1,3 +1,4 @@
+//M-1 T/C: O(N)
 void changeTree(BinaryTreeNode < int > * root) {
     // Write your code here.
 	if(root==nullptr){
@@ -26,4 +27,38 @@ void changeTree(BinaryTreeNode < int > * root) {
 		sum+= root->right->data;
 	}
 	if(root->left || root->right) root->data = sum;
+} 
+
+//M-2 T/C: O(n^2)
+void increment(BinaryTreeNode < int > * root, int val){
+	if(root->left){
+		root->left->data+= val;
+		increment(root->left, val);
+	}else if(root->right){
+		root->right->data+= val;
+		increment(root->right, val);
+	}
+}
+void changeTree(BinaryTreeNode < int > * root) {
+    // Write your code here.
+	if(root==nullptr || (root->left==nullptr && root->right==nullptr)){
+		return;
+	}
+	changeTree(root->left);
+	changeTree(root->right);
+	int tot = 0, diff;
+	if(root->left){
+		tot+= root->left->data;
+	}
+	if(root->right){
+		tot+= root->right->data;
+	}
+	diff = tot - root->data; 
+	
+	if(diff>=0){
+		root->data+= diff;
+	}else{
+		increment(root, -1*diff);
+	}
+	
 } 
