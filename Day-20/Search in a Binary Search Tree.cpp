76b2e6bae41ca909ee3d57033d86a807c1c26d3b
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-04 01:26:25 +0530

    Day 20: Q2

---
+++ Day-20/Search in a Binary Search Tree.cpp
@@ -0,0 +1,8 @@
+TreeNode* searchBST(TreeNode* root, int val) {
+        while(root!=nullptr){
+            if(root->val==val) return root;
+            else if(root->val>val) root = root->left;
+            else root = root->right;
+        }
+        return nullptr;
+    }
