0a5ce1648be287f2eaf16c90911026d8f7845228
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-27 15:17:26 +0530

    Day24: Q4

---
+++ Day-24/Floyd Warshall .cpp
@@ -0,0 +1,14 @@
+void shortest_distance(vector<vector<int>>&matrix){
+	    // Code here
+	    int n = matrix.size();
+	    for(int k=0; k<n; k++){
+	        for(int i=0; i<n; i++){
+	            for(int j=0; j<n; j++){
+	                if(i==j || i==k || j==k || matrix[i][k]==-1 || matrix[k][j]==-1){
+	                    continue;
+	                }
+	                matrix[i][j] = min(matrix[i][j]==-1?INT_MAX:matrix[i][j], matrix[i][k]+matrix[k][j]);
+	            }
+	        }
+	    }
+	}
