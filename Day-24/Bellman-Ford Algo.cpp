a5941fa7a1b1edba4f83dc7124771ab427165f86
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-27 15:16:58 +0530

    Day24: Q3

---
+++ Day-24/Bellman-Ford Algo.cpp
@@ -0,0 +1,15 @@
+ vector <int> bellman_ford(int V, vector<vector<int>> adj, int S) {
+        // Code here
+        int inf = 1e8;
+        vector<int> dist(V , inf);
+        dist[S]=0;
+        for(int i=0; i<V-1; i++){
+            for(auto x: adj){
+                int u = x[0], v = x[1], w = x[2];
+                if(dist[v]>dist[u]+w){
+                    dist[v] = dist[u]+w;
+                }
+            }
+        }
+        return dist;
+    }
