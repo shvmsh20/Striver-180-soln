ec3b4b27fb28d0ae5b617b04ab9ed620473d1a87
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-29 10:45:54 +0530

    update:Day25: Q1

--- Day-25/Max Product Subarray.cpp
+++ Day-25/Max Product Subarray.cpp
@@ -17,3 +17,41 @@ int maxProduct(vector<int>& nums) {
         }
         return res;
     }
+// M-2
+int maxProduct(vector<int>& nums) {
+        int res=INT_MIN;
+        int cpro=1;
+        for(int i=0; i<nums.size(); i++){
+            cpro*=nums[i];
+            res = max(res, cpro);
+            if(nums[i]==0){
+                cpro = 1;
+                continue;
+            }
+        }
+        cpro=1;
+        for(int i=nums.size()-1; i>=0; i--){
+            cpro*=nums[i];
+            res = max(res, cpro);
+            if(nums[i]==0){
+                cpro = 1;
+                continue;
+            }
+        }
+        return res;
+    }
+
+// M-1 modified  
+int maxProduct(vector<int>& nums) {
+        int res=nums[0];
+        int cmax = nums[0], cmin = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]<0){
+                swap(cmax, cmin);  
+            }
+            cmax = max(nums[i], cmax*nums[i]);
+            cmin = min(nums[i], cmin*nums[i]);
+            res = max(res, cmax);
+        }
+        return res;
+    }  
