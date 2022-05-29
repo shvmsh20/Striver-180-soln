16031b304d0144fbd4abb4ead08af9f1467cf766
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-30 00:10:47 +0530

    update: Day25: Q3

--- Day-25/Longest Common Subsequence.cpp
+++ Day-25/Longest Common Subsequence.cpp
@@ -62,3 +62,21 @@ int longestCommonSubsequence(string s1, string s2) {
         }
         return dp[n1][n2];
     }
+//Space optimation
+int longestCommonSubsequence(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        vector<int> curr(n2+1, 0);
+        vector<int> prev(n2+1, 0);
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                if(s1[i-1]==s2[j-1]){
+                    curr[j] = 1+prev[j-1];
+                }else{
+                    curr[j] = max(prev[j], curr[j-1]);
+                }
+            }
+            prev = curr;
+        }
+        return curr[n2];
+    }
