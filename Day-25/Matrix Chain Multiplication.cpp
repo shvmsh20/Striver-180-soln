336f41814b5616db35a271f0696667dc939a286f
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-02 17:51:03 +0530

    update: Day25: Q7

--- Day-25/Matrix Chain Multiplication.cpp
+++ Day-25/Matrix Chain Multiplication.cpp
@@ -86,4 +86,25 @@ int solve(int arr[], int i, int j, vector<vector<int>> &dp){
         // code here
         vector<vector<int>> dp(N, vector<int> (N, -1));
         return solve(arr, 1, N-1, dp);
+    }    
+
+//Bottom up
+int matrixMultiplication(int n, int arr[])
+    {
+        // code here
+        vector<vector<int>> dp(n, vector<int> (n));
+        for(int i=0; i<n; i++){
+            dp[i][i] = 0;
+        }
+        for(int i=n-1; i>=1; i--){
+            for(int j=i+1; j<n; j++){
+               int mn = INT_MAX;
+               for(int k=i; k<j; k++){
+                   int temp = (arr[i-1]*arr[k]*arr[j]) + dp[i][k]+dp[k+1][j];
+                   mn = min(mn, temp);
+               }
+               dp[i][j] = mn;
+            }
+        }
+        return dp[1][n-1];
     }        
