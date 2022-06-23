1515b450d790015361c4f9083ebed7233d6d411e
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-23 11:57:05 +0530

    update: 23 June 2022

--- Day-2/Find_the_Duplicate_Number.cpp
+++ Day-2/Find_the_Duplicate_Number.cpp
@@ -1,7 +1,17 @@
 M-1 Sort 
 M-2 Hashing
 M-3 tortoise method
-
+M-4 using negative numbers
+int findDuplicate(vector<int>& nums) {
+        int curr = 0;
+        while(nums[curr]>0){
+            int next = nums[curr];
+            nums[curr] = -nums[curr];
+            curr = next;
+        }
+        return curr;
+    }
+    
 int findDuplicate(vector<int>& nums) {
         int fast = nums[0], slow = nums[0];
         //find collision point
