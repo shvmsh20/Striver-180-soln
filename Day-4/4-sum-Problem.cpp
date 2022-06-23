efd469459405498d8e266c4d6ab73e7c28a8a117
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-24 03:12:59 +0530

    update: Day-4: Q2

--- Day-4/4-sum-Problem.cpp
+++ Day-4/4-sum-Problem.cpp
@@ -1,3 +1,53 @@
+//Accepted
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int lo = j+1;
+                int hi = n-1;
+                if((long long)target-nums[i]<INT_MIN){
+                    break;;
+                }
+                long long temp = target-nums[i];
+                if((long long)temp-nums[j]<INT_MIN){
+                    break;
+                }
+                temp-= nums[j];
+                vector<int> v(4);
+                while(lo<hi){
+                    if(nums[lo]+nums[hi]==temp){
+                        v[0] = nums[i];
+                        v[1] = nums[j];
+                        v[2] = nums[lo];
+                        v[3] = nums[hi];
+                        res.push_back(v);
+                        while(lo<hi && nums[lo]==nums[lo+1]){
+                            lo++;
+                        }
+                        while(lo<hi && nums[hi]==nums[hi-1]){
+                            hi--;
+                        }
+                        lo++;
+                        hi--;
+                    }else if(nums[lo]+nums[hi]<temp){
+                        lo++;
+                    }else{
+                        hi--;
+                    }
+                }
+                while(j+1<n && nums[j]==nums[j+1]){
+                    j++;
+                }
+            }
+            while(i+1<n && nums[i]==nums[i+1]){
+                i++;
+            }
+        }
+        return res;
+    }
+//Not Accepted now
 vector<vector<int>> fourSum(vector<int>& num, int target) {
         vector<vector<int> > res;
         
