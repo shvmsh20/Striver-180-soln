9101441cb3baca077c3fff17802054b6a4c159b0
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-23 18:29:39 +0530

    update: Day-3: Q2

--- Day-3/Pow(X,n).cpp
+++ Day-3/Pow(X,n).cpp
@@ -1,25 +1,35 @@
 // T/C = O(logn)
 
 //Using recursion
-double util(double x, long long n){
+
+double solve(double x, int n){
         if(n==0){
             return 1;
-        }
-        if(n%2==0){
-            double temp = util(x, n/2);
-            return temp*temp;
+        }else if(n==1){
+            return x;
+        }else if(n%2==0){
+            double t = solve(x, n/2);
+            return t*t;
         }else{
-            return x*util(x, n-1);
+            return x*solve(x, n-1);
         }
     }
-    double myPow(double x, long long n) {
+    double myPow(double x, int n) {
         if(n<0){
-            n *=-1;
-            return (1/util(x, n));
+            //handling special case when n = INT_MIN, so it cannot be converted into 
+            //positive integer
+            if(n==INT_MIN){
+                return 1/(solve(x, INT_MAX)*x);
+            }
+            else{
+                n = abs(n);
+                return 1/solve(x, n);
+            }
         }else{
-            return util(x, n);
+            return solve(x, n);
         }
     }
+
 
 //Using loop
 double myPow(double x, int n) {
