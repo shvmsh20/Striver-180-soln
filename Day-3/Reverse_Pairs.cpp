f623440d529dc635f5d40c512b9297a1d50fe52c
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-24 00:53:39 +0530

    update: Day-3: Q6

--- Day-3/Reverse_Pairs.cpp
+++ Day-3/Reverse_Pairs.cpp
@@ -45,3 +45,50 @@ void merge(vector<int> &nums, int lo, int mid, int hi, int &count){
         mergeSort(nums, 0, n-1, count);
         return count;
     }
+
+
+//Different Implementation
+    int merge(vector<int>& nums, vector<int> &temp, int lo, int mid, int hi){
+        int i=lo, j=mid+1, k=lo;
+        int count=0;
+        for(int i=lo; i<=mid; i++){
+            while(j<=hi && nums[i]>2LL*nums[j]){
+                j++;
+            }
+            count+= j-(mid+1);
+        }
+        j=mid+1;
+        while(i<=mid && j<=hi){
+            if(nums[i]<=nums[j]){
+                temp[k++] = nums[i++];
+            }else{
+                temp[k++] = nums[j++];
+            }
+        }
+        while(i<=mid){
+            temp[k++] = nums[i++];
+        }
+        while(j<=hi){
+            temp[k++] = nums[j++];
+        }
+        for(int i=lo; i<=hi; i++){
+            nums[i] = temp[i];
+        }
+        return count;
+    }
+    int mergeSort(vector<int>& nums, vector<int> &temp, int lo, int hi){
+        if(hi>lo){
+            int mid = lo+(hi-lo)/2;
+            int count=0;
+            count+= mergeSort(nums, temp, lo, mid);
+            count+= mergeSort(nums, temp, mid+1, hi);
+            count+= merge(nums, temp, lo, mid, hi);
+            return count;  
+        }
+        return 0;
+    }
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp(n);
+        return mergeSort(nums, temp, 0, n-1);
+    }
