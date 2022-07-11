e97da4b201fb4f1d6f598307e0c669b540fd520a
shvmsh20 <itsshivamsharma21@gmail.com>
2022-07-11 19:53:00 +0530

    update: Day13: Q2

--- Day-13/Implement Queue Using Arrays.cpp
+++ Day-13/Implement Queue Using Arrays.cpp
@@ -1,45 +1,19 @@
-vector<int> v(100);
-    int front;
-    int rear;
-    int size;
-    Queue() {
-        // Implement the Constructor
-        size=0;
-        front=0;
-        rear=0;
-    }
-
-    /*----------------- Public Functions of Queue -----------------*/
-
-    bool isEmpty() {
-        // Implement the isEmpty() function
-        
-        if(size==0){
-            return true;
-        }else {
-            return false;
-        }
-    }
-
-    void enqueue(int data) {
-        // Implement the enqueue() function
-        v[(rear+1)%100] = data;
-        rear = rear++;
-        size++;
-    }
-
-    int dequeue() {
+int dequeue() {
         // Implement the dequeue() function
-        if(size==0) retun -1;
-        int temp = v[front%100];
-        front = front--;
-        size--;
-        return temp;
-        
+        if(sz==0){
+            return -1;
+        }
+        int ans = q[frnt%n];
+        frnt++;
+        sz--;
+        return ans;
     }
 
     int front() {
         // Implement the front() function
-        if(size==0) return -1;
-        return v[front];
+        if(sz==0){
+            return -1;
+        }
+        return q[frnt%n];
     }
+};
