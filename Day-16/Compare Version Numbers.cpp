6babd14f015024fda5b0d5caf4ffa670e7835151
shvmsh20 <itsshivamsharma21@gmail.com>
2022-07-16 23:26:19 +0530

    update: Day16: Q6

--- Day-16/Compare Version Numbers.cpp
+++ Day-16/Compare Version Numbers.cpp
@@ -1,70 +1,26 @@
-int compareVersion(string version1, string version2) {
-        int n = version1.length();
-        int m = version2.length();
-        int i=0, j=0;
-        while(i<n && j<m){
-            string r1="";
-            while(i<n && version1[i]!='.'){
-                r1 = r1+version1[i];
+int compareVersion(string v1, string v2) {
+        int n = v1.length(), i=0;
+        int m = v2.length(), j=0;
+        while(i<n || j<m){
+            int n1=0, n2=0;
+            while(i<n && v1[i]!='.'){
+                n1 = n1*10 + (v1[i]-'0');
                 i++;
             }
-            i++;
-            int t1 = stoi(r1);
-            
-            string r2="";
-            while(j<m && version2[j]!='.'){
-                r2 = r2+version2[j];
+            while(j<m && v2[j]!='.'){
+                n2 = n2*10 + (v2[j]-'0');
                 j++;
             }
-            j++;
-            int t2 = stoi(r2);
-            cout << t1 << " " << t2 << endl;
-            if(t1<t2){
-                return -1;
-            }else if(t1>t2){
-                return 1;
-            }else{
-                continue;
-            }
-        }
-        while(i<n){
-            string r1="";
-            while(i<n && version1[i]!='.'){
-                r1 = r1+version1[i];
-                i++;
-            }
             i++;
-            int t1 = stoi(r1);
-            int t2 = 0;
-            cout << t1 << " " << t2 << endl;
-            if(t1<t2){
-                return -1;
-            }else if(t1>t2){
-                return 1;
-            }
-        }
-        while(j<m){
-            string r2="";
-            while(j<m && version2[j]!='.'){
-                r2 = r2+version2[j];
-                j++;
-            }
             j++;
-            int t1 = 0;
-            int t2 = stoi(r2);
-            cout << t1 << " " << t2 << endl;
-            if(t1<t2){
+            if(n1<n2){
                 return -1;
-            }else if(t1>t2){
+            }else if(n1>n2){
                 return 1;
             }
         }
-        
         return 0;
-            
     }
-
-
     //OR
     int compareVersion(string v1, string v2) {
         int n = v1.length(), i=0;
