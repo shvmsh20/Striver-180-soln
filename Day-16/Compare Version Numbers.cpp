49dfd18c0eccdb7f57dbb302a937f2b01e5cdcd9
shvmsh20 <itsshivamsharma21@gmail.com>
2022-07-16 23:11:33 +0530

    update: Day16: Q6

--- Day-16/Compare Version Numbers.cpp
+++ Day-16/Compare Version Numbers.cpp
@@ -63,3 +63,55 @@ int compareVersion(string version1, string version2) {
         return 0;
             
     }
+
+
+    //OR
+    int compareVersion(string v1, string v2) {
+        int n = v1.length(), i=0;
+        int m = v2.length(), j=0;
+        while(i<n || j<m){
+            string rev1="", rev2="";
+            while(i<n && v1[i]!='.' && v1[i]=='0'){
+                i++;
+            }
+            if(i<n){
+                if(v1[i]=='.'){
+                    rev1="0";
+                }else{
+                    while(i<n && v1[i]!='.'){
+                        rev1+= v1[i];
+                        i++;
+                    }
+                }
+                i++;
+            }else{
+                rev1='0';
+            }
+            
+            
+            while(j<m && v2[j]!='.' && v2[j]=='0'){
+                j++;
+            }
+            if(j<m){
+                if(v2[j]=='.'){
+                    rev2="0";
+                }else{
+                    while(j<m && v2[j]!='.'){
+                        rev2+= v2[j];
+                        j++;
+                    }
+                }
+                j++;
+            }else{
+                rev2='0';
+            }
+            
+            if(stoi(rev1)<stoi(rev2)){
+                return -1;
+            }else if(stoi(rev1)>stoi(rev2)){
+                return 1;
+            }
+            
+        }
+        return 0;
+    }
