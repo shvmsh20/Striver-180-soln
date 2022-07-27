a5dbaade40534a39160014910774d61e1e5318ce
shvmsh20 <itsshivamsharma21@gmail.com>
2022-07-27 09:29:12 +0530

    update: Day23: Q4

--- Day-23/BFS-Detect cycle in an undirected graph.cpp
+++ Day-23/BFS-Detect cycle in an undirected graph.cpp
@@ -1,37 +1,31 @@
-bool cyclebfs(int u, vector<int> adj[], vector<bool> &visited){
-        visited[u]=true;
-        queue<pair<int, int>>q;
+bool bfs(int u, vector<int> adj[], vector<bool> &vis){
+        queue<pair<int, int> > q;
         q.push({u, -1});
+        vis[u] = true;
         while(!q.empty()){
-            int n = q.size();
-            while(n--){
-                pair<int, int> p = q.front();
-                q.pop();
-                int x = p.first;
-                int y = p.second;
-                for(int v: adj[x]){
-                    if(visited[v]==false){
-                        visited[v] = true;
-                        q.push({v, x});
-                    }else{
-                        if(v!=y){
-                            return true;
-                        }
+            int x = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            for(int v: adj[x]){
+                if(vis[v]==true){
+                    if(v!=parent){
+                        return true;
                     }
+                    
+                }else{
+                    vis[v] = true;
+                    q.push({v, x});
                 }
             }
         }
         return false;
-        
     }
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
-        vector<bool> visited(V, false);
+        vector<bool> vis(V, false);
         for(int i=0; i<V; i++){
-            if(visited[i]==false){
-                if(cyclebfs(i, adj, visited)){
-                    return true;
-                }
+            if(vis[i]==false && bfs(i, adj, vis)){
+                return true;
             }
         }
         return false;
